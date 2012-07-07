31c9322bbbc1e1d67a426fc7d97ca5ec86f2ef96
Duncan McGregor <duncan@oneeyedmen.com>
2012-07-07 18:33:38 +0100

    Initial commit

---
+++ src/raspwitch.py
@@ -0,0 +1,54 @@
+import time
+
+preamble = [0] * 26
+postamble = [0] * 5
+
+channel_codes = [
+    [859124533, 861090613, 892547893, 1395864373],
+    [859124563, 861090643, 892547923, 1395864403],
+    [859125043, 861091123, 892548403, 1395864883],
+    [859132723, 861098803, 892556083, 1395872563]
+]
+
+on_code = 13107
+off_code = 21299
+
+default_pulse_width =500 * 1e-6
+
+# converts the lowest bit_count bits to a list of ints
+def int_to_bit_list(i, bit_count):
+    result = []
+    shifted = i
+    for i in range(0, bit_count):
+        result.append(shifted & 0x01)
+        shifted = shifted >> 1
+    return result
+
+# encodes 0 as a 1 count state change, 1 as a 3 count state change
+def encode_as_state_list(bit_list):
+    result = []
+    state = 1
+    for bit in bit_list:
+        result.append(state)
+        if bit == 1:
+            result.append(state)
+            result.append(state)
+        state = 1 - state
+    return result
+
+def encode_packet(bit_list):
+    return preamble + [1] + encode_as_state_list(bit_list) + postamble
+
+def command_as_bit_list(channel, button, on):
+    return int_to_bit_list(
+        channel_codes[channel - 1][button - 1], 32) + \
+        int_to_bit_list(on_code if on else off_code, 16)
+
+def send(pin, state_list, pulse_width, my_time = time):
+    for state in state_list:
+        pin.value = state
+        my_time.sleep(pulse_width)
+
+def send_command(pin, channel, button, on, pulse_width = default_pulse_width):
+    send(pin, encode_packet(command_as_bit_list(channel, button, on)), pulse_width)
+
---
+++ src/test.py
@@ -0,0 +1,37 @@
+import unittest
+from raspwitch import *
+
+class Test(unittest.TestCase):
+
+    def test_int_to_bit_list(self):
+        self.assertEqual([0], int_to_bit_list(0, 1))
+        self.assertEqual([1], int_to_bit_list(1, 1))
+        self.assertEqual([1, 0], int_to_bit_list(1, 2))
+        self.assertEqual([0, 1], int_to_bit_list(2, 2))
+        self.assertEqual([1, 1, 0, 0], int_to_bit_list(3, 4))
+
+    def test_encode_as_state_list(self):
+        self.assertEqual([1], encode_as_state_list([0]))
+        self.assertEqual([1, 1, 1], encode_as_state_list([1]))
+        self.assertEqual([1, 0, 0, 0], encode_as_state_list([0, 1]))
+        self.assertEqual([1, 1, 1, 0], encode_as_state_list([1, 0]))
+
+    def test_encode_packet(self):
+        preamble = [0] * 26
+        postamble = [0] * 5
+
+        self.assertEquals(preamble + [1] + postamble, encode_packet([]))
+        self.assertEquals(preamble + [1] + [1, 0, 0, 0] + postamble, encode_packet([0, 1]))
+
+    def test_command_as_bit_list(self):
+        self.assertEquals(int_to_bit_list(channel_codes[0][0], 32) + int_to_bit_list(on_code, 16),
+            command_as_bit_list(1, 1, True))
+        self.assertEquals(int_to_bit_list(channel_codes[1][2], 32) + int_to_bit_list(off_code, 16),
+            command_as_bit_list(2, 3, False))
+
+    def test_send(self):
+        capturingPin = lambda: None # get around to this
+        send(capturingPin, [0, 1, 1, 1, 0], 0.005)
+
+if __name__ == "__main__":
+    unittest.main()
