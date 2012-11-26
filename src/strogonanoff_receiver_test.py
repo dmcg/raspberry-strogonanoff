eec4a6f797d9949f4d446a79cdb5ac1f1a14443f
Duncan McGregor <duncan@oneeyedmen.com>
2012-11-26 21:52:07 +0000

    test accumulator

--- src/strogonanoff_receiver_test.py
+++ src/strogonanoff_receiver_test.py
@@ -59,14 +59,32 @@ class StateMachine:
 
 class Accumulator:
 
+    def __init__(self, callback):
+        self.callback = callback
+        self.reset()
+
     def on_short(self, level):
-        p("*")
+        self._new_bit(0)
 
     def on_long(self, level):
-        p("***")
+        self._new_bit(1)
+
+    def get_command(self):
+        return self.result & 0xffffffff
+
+    def get_state_code(self):
+        return self.result >> 32
 
     def reset(self):
-        p("X")
+        self.result = long(0)
+        self.bit_count = 0
+
+    def _new_bit(self, bit):
+        self.result = self.result | (bit & 0x01) << self.bit_count
+        self.bit_count = self.bit_count + 1
+        if self.bit_count == 48:
+            self.callback(self.get_command(), self.get_state_code())
+            self.reset()
 
 def poll(pin, callback):
     level = 0
@@ -79,17 +97,62 @@ def poll(pin, callback):
             start_t = now
 
 
+from strogonanoff_sender import *
+
+def run_lengths(list):
+    result = []
+    if len(list) == 0:
+        return result
+
+    previous = list[0]
+    count = 1
+    for e in list[1:]:
+        if e == previous:
+            count = count + 1
+        else:
+            result.append((previous, count))
+            previous = e
+            count = 1
+    result.append((previous, count))
+    return result
+
+
+def pass_state_list_to_accumulator(list, accumulator):
+    for level_len in run_lengths(list):
+        if level_len[1] == 1:
+            accumulator.on_short(level_len[0])
+        elif level_len[1] == 3:
+            accumulator.on_long(level_len[0])
+        else:
+            raise "bad run length"
+
+
+def callback(channel, button, state):
+    print channel, button, state
+
 class Strogonanoff_ReceiverTest(unittest.TestCase):
 
-    def test_poll(self):
+    def xTest_poll(self):
         from WiringPin import WiringPin
         pin = WiringPin(0, "in")
-        accumulator = Accumulator()
+        accumulator = Accumulator(callback)
         state_machine = StateMachine(accumulator)
         poll(pin, state_machine.on_pulse)
 
-    def test_accumulate(self):
-        pass
+    def test_accumulator(self):
+        channel = 4
+        button = 2
+
+        def callback(command, state):
+            self.found_command = command
+            self.found_state = state
+
+        accumulator = Accumulator(callback)
+        state_changes = encode_as_state_list(command_as_bit_list(channel, button, True))
+        pass_state_list_to_accumulator(state_changes, accumulator)
+        self.assertEquals(channel_codes[channel - 1][button - 1], self.found_command)
+        self.assertEquals(on_code, self.found_state)
+
 
 if __name__ == "__main__":
     unittest.main()
